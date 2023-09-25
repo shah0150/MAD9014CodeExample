9edafdcf09f329c0d4e0dec2266a71561562322f
Adesh Shah <adeshshah@Adeshs-Laptop.hitronhub.home>
2023-09-25 18:55:51 -0400

    baisc array

---
+++ week-4/basicArrays.js
@@ -0,0 +1,41 @@
+// let numbers = [1, 2, 3, 4, 5]
+
+// console.log(numbers);
+
+// let fruits = ["Apples", "Strawberry", "Cherry"];
+// console.log(fruits);
+
+// let mixedArray = [1, "Apples", true, function(){}];
+// console.log(mixedArray);
+
+// console.log(fruits[2]);
+
+// fruits[1] = "Banana";
+// console.log(fruits);
+
+// let fruits = ["Apples", "Strawberry", "Cherry"]
+// fruits.push("Apricot");
+// fruits.pop();
+// console.log(fruits);
+
+// let fruits = ["Apples", "Strawberry", "Cherry"];
+// fruits.shift();
+// console.log(fruits);
+// fruits.unshift("Apples");
+// console.log(fruits);
+
+// let fruits = ["Apples", "Strawberry", "Cherry"];
+// fruits.push("Mango", "Pineapple", "Peach");
+// console.log(fruits);
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+// let fruits = ["Apples", "Oranges", "Cherry"];
+ 
+// for (let i =0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
