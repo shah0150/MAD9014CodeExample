4a573ddfb605a6fcc56a5f6dbaa9cad63138162b
Adesh Shah <shah0150@algonquinlive.com>
2023-11-16 17:54:38 -0500

    merge

--- week-11-1/cookies/main.js
+++ week-11-1/cookies/main.js
@@ -1,53 +1,65 @@
-// Function to set a cookie
+// Function to set Cookies
 function setCookie(name, value, daysToLive) {
     let expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + daysToLive);
-    let cookieString = name + '=' + encodeURIComponent(value) + '; expires=' + expirationDate.toUTCString() + '; path=/';
-    document.cookie = cookieString;
+    expirationDate.setDate(expirationDate.getDate() + daysToLive)
+    let cookieString = name + '=' + encodeURIComponent(value) + '; expires=' + expirationDate.toUTCString() + '; path=/'
+
+    document.cookie = cookieString
 }
 
-// Function to get a cookie
-function getCookie(name) {
-    let cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i].trim();
-        if (cookie.indexOf(name + '=') === 0) {
-            return decodeURIComponent(cookie.substring(name.length + 1));
-        }
-    }
-    return null;
-}
+let setCookieButton = document.querySelector('#setCookies');
+setCookieButton.addEventListener('click', function(){
+    // Set cookie named username with value of "your-name" that expires in 7 days.
 
-// Function to delete a cookie by name
-function deleteCookie(name) {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-}
-
-
-
-let setCookieButton = document.getElementById('setCookies');
-setCookieButton.addEventListener('click', function () {
-    // Set a cookie named 'username' with the value 'John' that expires in 7 days
-    setCookie('username', 'Arianne', 7);
-
-    // Display a message or update the UI as needed
-    let outputElement = document.getElementById('output');
-    outputElement.innerText = 'Cookie set!';
-
-});
-
-let getCookieButton = document.getElementById('getCookies')
-getCookieButton.addEventListener('click', function (){
-    let outputElement = document.getElementById('output');
-    let username = getCookie('username');
-    outputElement.innerText = 'Welcome back, ' + username + '!';
-
-});
-
-let delCookieButton = document.getElementById('delCookies')
-delCookieButton.addEventListener('click', function (){
-    let username = deleteCookie('username');
-    let outputElement = document.getElementById('output');
-    outputElement.innerText = "Cookie " + username + " Successfully Deleted" + "!";
+    setCookie('username', 'Adesh', 7)
+    let output = document.getElementById('output')
+    output.innerText = 'Cookies Set'
 })
 
+// Function to get Cookies
+
+function getCookie(name) {
+    let cookies = document.cookie.split(';')
+    for (let i = 0; i < cookies.length; i++){
+        let cookie = cookies[i].trim()
+        console.log(cookie);
+
+        if (cookie.indexOf(name + '=') === 0){
+            return decodeURIComponent(cookie.substring(name.length + 1))
+        }
+    }
+    return;
+}
+
+let getCookieButton = document.getElementById('getCookies')
+getCookieButton.addEventListener('click', function() {
+    let output = document.getElementById('output')
+    let username = getCookie('username')
+    output.innerText = 'Welcome back, ' + username + '!'
+})
+
+// fucntion to delete cookie by name
+
+function deleteCookie(name){
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/'
+}
+
+let delCookie = document.getElementById('delCookies')
+delCookie.addEventListener('click', function(){
+    let username = deleteCookie('username')
+    let output = document.getElementById('output')
+    output.innerText = "Cookie " + username + " successfully deleted!"
+})
+
+function deleteCookie(name) {
+    let delCookies = document.cookie.split(';')
+    for (let i = 0; i < delCookies.length; i++){
+        let delCookie = delCookies[i].trim()
+        console.log(delCookie);
+
+        if (delCookie.indexOf(name + '=') === 0){
+            return decodeURIComponent(delCookie.substring(name.length + 1))
+        }
+    }
+    return;
+}
